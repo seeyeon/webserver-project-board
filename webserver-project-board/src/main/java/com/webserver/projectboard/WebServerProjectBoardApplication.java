e5312a02bcf4539f9856ab06a63175fd7d5a23da
seeyeon <71398979+seeyeon@users.noreply.github.com>
2022-12-16 03:50:02 +0900

    #7-spring boot 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 기술은 : 
    
    * spring web
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ webserver-project-board/src/main/java/com/webserver/projectboard/WebServerProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.webserver.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerProjectBoardApplication.class, args);
+    }
+
+}
---
+++ webserver-project-board/src/test/java/com/webserver/projectboard/WebServerProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webserver.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
