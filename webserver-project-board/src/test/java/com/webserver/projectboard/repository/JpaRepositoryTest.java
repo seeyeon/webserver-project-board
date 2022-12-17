7db96d9895e70d82fcd87f6c0661a98af6fbe2e7
seeyeon <71398979+seeyeon@users.noreply.github.com>
2022-12-18 05:53:21 +0900

    #10 - JPA TEST 작성
    기본 CRUD &  연관관계 매핑과 cascading이 잘 동작하는지 test함.
    테스트 데이터와 관련이 있기 때문에 이대로는 실패한다.

---
+++ webserver-project-board/src/test/java/com/webserver/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.webserver.projectboard.repository;
+
+import com.webserver.projectboard.config.JpaConfig;
+import com.webserver.projectboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA connection Test")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository)
+    {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("delete Test")
+       @Test
+       void givenTestData_whenDeleting_thenWorksFine() {
+           //Given
+           Article article = articleRepository.findById(1L).orElseThrow();
+           long previousArticleCount = articleRepository.count();
+           long previousArticleCommentCount = articleCommentRepository.count();
+           int deletedCommentsSize = article.getArticleComments().size();
+
+
+           //When
+           articleRepository.delete(article);
+
+           //Then
+           assertThat(articleRepository.count()).isEqualTo(previousArticleCount-1);
+           assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount-deletedCommentsSize);
+    }
+
+}
