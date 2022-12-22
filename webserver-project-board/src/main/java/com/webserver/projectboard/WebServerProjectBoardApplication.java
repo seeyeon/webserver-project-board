725eea82cc0f065aa103ed478551eda3cc7c2c06
seeyeon <71398979+seeyeon@users.noreply.github.com>
2022-12-23 05:40:13 +0900

    #16 - Thymeleaf3 decoupled logic 설정
    java code로 Thymeleaf3 decoupled logic  을 사용자 application property로 제어할 수 있게 설정 추가.

--- webserver-project-board/src/main/java/com/webserver/projectboard/WebServerProjectBoardApplication.java
+++ webserver-project-board/src/main/java/com/webserver/projectboard/WebServerProjectBoardApplication.java
@@ -2,7 +2,9 @@ package com.webserver.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class WebServerProjectBoardApplication {
 
